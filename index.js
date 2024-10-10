8c25513e8ba1bd23f9531ca1a66a0c6ac29bc179
XavMontana <xavierhertzog@gmail.com>
2024-10-10 11:16:04 -0400

    I am up to function 2

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 //write your code here
+reverseString = (str) => {
+  let reverseString = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reverseString += str[i];
+  }
+  return reverseString;
+};
+console.log(reverseString("hello"));
+console.log(reverseString("Holo Como estas"));
+
+reverseZigZagString = (str) => {
+    let reverseZigZagString = "";
+    for(let i)
+     
+    
+}
